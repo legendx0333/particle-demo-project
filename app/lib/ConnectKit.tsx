d196b22b4b494086b999e057536377f8ed901dd4
Soos3D <99700157+soos3d@users.noreply.github.com>
2025-02-10 12:31:07 -0300

    fixes
    Use base chain object from Connectkit
    
    Send batched gasless tx

--- app/lib/ConnectKit.tsx
+++ app/lib/ConnectKit.tsx
@@ -1,90 +1,95 @@
-'use client';
+"use client";
 
-import { ConnectKitProvider, createConfig } from '@particle-network/connectkit';
-import { aa } from '@particle-network/connectkit/aa';
-import { authWalletConnectors } from '@particle-network/connectkit/auth';
-import { defineChain, polygon, base } from '@particle-network/connectkit/chains';
-import { evmWalletConnectors } from '@particle-network/connectkit/evm';
-import { EntryPosition, wallet } from '@particle-network/connectkit/wallet';
-import React from 'react';
+import { ConnectKitProvider, createConfig } from "@particle-network/connectkit";
+import { aa } from "@particle-network/connectkit/aa";
+import { authWalletConnectors } from "@particle-network/connectkit/auth";
+import {
+  defineChain,
+  //polygon,
+  base,
+} from "@particle-network/connectkit/chains";
+import { evmWalletConnectors } from "@particle-network/connectkit/evm";
+import { EntryPosition, wallet } from "@particle-network/connectkit/wallet";
+import React from "react";
 
 //Retrived from https://dashboard.particle.network
 const projectId = process.env.NEXT_PUBLIC_PARTICLE_PROJECT_ID!;
 const clientKey = process.env.NEXT_PUBLIC_PARTICLE_CLIENT_KEY!;
 const appId = process.env.NEXT_PUBLIC_PARTICLE_APP_ID!;
-const walletConnectProjectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
+const walletConnectProjectId =
+  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!;
 const VLR_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_VLR_CONTRACT_ADDRESS;
-const NFT_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS;
+//const NFT_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS;
 
 if (!projectId || !clientKey || !appId) {
-    throw new Error('Please configure the Particle project in .env first!');
+  throw new Error("Please configure the Particle project in .env first!");
 }
 
 const customPolygon = defineChain({
-    id: base.id,
-    name: base.name,
-    nativeCurrency: base.nativeCurrency,
-    blockExplorers: base.blockExplorers,
-    rpcUrls: {
-        default: {
-            http: [process.env.NEXT_PUBLIC_POLYGON_RPC_URL!],
-            ws: [process.env.NEXT_PUBLIC_POLYGON_WS_URL!],
-        }
+  id: base.id,
+  name: base.name,
+  nativeCurrency: base.nativeCurrency,
+  blockExplorers: base.blockExplorers,
+  rpcUrls: {
+    default: {
+      http: [process.env.NEXT_PUBLIC_POLYGON_RPC_URL!],
+      ws: [process.env.NEXT_PUBLIC_POLYGON_WS_URL!],
     },
-})
+  },
+});
 
 const config = createConfig({
-    projectId,
-    clientKey,
-    appId,
-    appearance: {
-        // Optional, collection of properties to alter the appearance of the connection modal
-        // Optional, label and sort wallets (to be shown in the connection modal)
-        recommendedWallets: [
-            { walletId: 'metaMask', label: 'Recommended' },
-            { walletId: 'coinbaseWallet', label: 'popular' },
-        ],
-        language: 'en-US', // Optional, also supported ja-JP, zh-CN, zh-TW, and ko-KR
-    },
-    walletConnectors: [
-        evmWalletConnectors({
-            metadata: { name: 'My App', icon: '', description: '', url: '' }, // Optional, this is Metadata used by WalletConnect and Coinbase
-            walletConnectProjectId: walletConnectProjectId, // optional, retrieved from https://cloud.walletconnect.com
-        }),
-        authWalletConnectors({
-            // Optional, configure this if you're using social logins
-            authTypes: ['email', 'google', 'apple', 'twitter', 'github'], // Optional, restricts the types of social logins supported
-            fiatCoin: 'USD', // Optional, also supports CNY, JPY, HKD, INR, and KRW
-            promptSettingConfig: {
-                // Optional, changes the frequency in which the user is asked to set a master or payment password
-                // 0 = Never ask
-                // 1 = Ask once
-                // 2 = Ask always, upon every entry
-                // 3 = Force the user to set this password
-                promptMasterPasswordSettingWhenLogin: 1,
-                promptPaymentPasswordSettingWhenSign: 1,
-            },
-        }),
+  projectId,
+  clientKey,
+  appId,
+  appearance: {
+    // Optional, collection of properties to alter the appearance of the connection modal
+    // Optional, label and sort wallets (to be shown in the connection modal)
+    recommendedWallets: [
+      { walletId: "metaMask", label: "Recommended" },
+      { walletId: "coinbaseWallet", label: "popular" },
     ],
-    plugins: [
-        wallet({
-            // Optional configurations for the attached embedded wallet modal
-            entryPosition: EntryPosition.MC, // Alters the position in which the modal button appears upon login
-            visible: true, // Dictates whether or not the wallet modal is included/visible or not
-            customStyle: {
-                displayTokenAddresses: [VLR_CONTRACT_ADDRESS], // Display a custom token within the wallet modal
-                priorityTokenAddresses: [VLR_CONTRACT_ADDRESS],
-            },
-        }),
-        aa({
-            name: 'SIMPLE',
-            version: '2.0.0',
-        })
-    ],
-    chains: [customPolygon],
+    language: "en-US", // Optional, also supported ja-JP, zh-CN, zh-TW, and ko-KR
+  },
+  walletConnectors: [
+    evmWalletConnectors({
+      metadata: { name: "My App", icon: "", description: "", url: "" }, // Optional, this is Metadata used by WalletConnect and Coinbase
+      walletConnectProjectId: walletConnectProjectId, // optional, retrieved from https://cloud.walletconnect.com
+    }),
+    authWalletConnectors({
+      // Optional, configure this if you're using social logins
+      authTypes: ["email", "google", "apple", "twitter", "github"], // Optional, restricts the types of social logins supported
+      fiatCoin: "USD", // Optional, also supports CNY, JPY, HKD, INR, and KRW
+      promptSettingConfig: {
+        // Optional, changes the frequency in which the user is asked to set a master or payment password
+        // 0 = Never ask
+        // 1 = Ask once
+        // 2 = Ask always, upon every entry
+        // 3 = Force the user to set this password
+        promptMasterPasswordSettingWhenLogin: 1,
+        promptPaymentPasswordSettingWhenSign: 1,
+      },
+    }),
+  ],
+  plugins: [
+    wallet({
+      // Optional configurations for the attached embedded wallet modal
+      entryPosition: EntryPosition.BR, // Alters the position in which the modal button appears upon login
+      visible: true, // Dictates whether or not the wallet modal is included/visible or not
+      customStyle: {
+        displayTokenAddresses: [VLR_CONTRACT_ADDRESS], // Display a custom token within the wallet modal
+        priorityTokenAddresses: [VLR_CONTRACT_ADDRESS],
+      },
+    }),
+    aa({
+      name: "BICONOMY",
+      version: "2.0.0",
+    }),
+  ],
+  chains: [base],
 });
 
 // Export ConnectKitProvider to be used within your index or layout file (or use createConfig directly within those files).
 export const ParticleConnectkit = ({ children }: React.PropsWithChildren) => {
-    return <ConnectKitProvider config={config}>{children}</ConnectKitProvider>;
+  return <ConnectKitProvider config={config}>{children}</ConnectKitProvider>;
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,75 @@
 "use client";
-import { ConnectButton, useAccount, useAddress, useSmartAccount, useConnect } from "@particle-network/connectkit";
-import { AAWrapProvider, SendTransactionMode, Transaction } from "@particle-network/aa";
-import { type Eip1193Provider, ethers, parseUnits } from 'ethers';
+import {
+  ConnectButton,
+  useAccount,
+  //useAddress,
+  useSmartAccount,
+} from "@particle-network/connectkit";
+import {
+  AAWrapProvider,
+  SendTransactionMode,
+  Transaction,
+} from "@particle-network/aa";
+import { type Eip1193Provider, ethers, parseUnits } from "ethers";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import ABI from './ABI.json';
-import VLRABI from './VLRABI.json';
+import ABI from "./ABI.json";
+import VLRABI from "./VLRABI.json";
 import { Interface } from "ethers";
 
 const NFT_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS!;
 const VLR_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_VLR_CONTRACT_ADDRESS!;
 
-const DEFAULT_IPFS_URI = "ipfs://QmYzp9Rnt9YUSujqJwauLH7SRThEz63jfqDhAaaW1VFuLN";
-const SENDER_ADDRESS = "0xEF81ef2493525EC812F386465059D029691B8764";
+const DEFAULT_IPFS_URI =
+  "ipfs://QmYzp9Rnt9YUSujqJwauLH7SRThEz63jfqDhAaaW1VFuLN";
+//const SENDER_ADDRESS = "0xEF81ef2493525EC812F386465059D029691B8764";
+const RECEIVER_ADDRESS = "0xf25DADF841518A2cb516307876CE44F416661085"; // EDIT THIS
 
-console.log("VLR_CONTRACT_ADDRESS: ", VLR_CONTRACT_ADDRESS)
+console.log("VLR_CONTRACT_ADDRESS: ", VLR_CONTRACT_ADDRESS);
 
 export default function Home() {
   const { isConnected } = useAccount();
-  const address = useAddress();
+  //const address = useAddress();
+
   const [signer, setSigner] = useState<ethers.Signer | null>(null);
-  const { connect } = useConnect();
+  const [smartAccountAddress, setSmartAccountAddress] = useState<string | null>(
+    null
+  );
+
+  const [txsString, setTxsString] = useState<string | null>(null);
+  const [feeQuotesString, setFeeQuotesString] = useState<string | null>(null);
 
   const smartAccount = useSmartAccount();
 
-  const provider = useMemo(() => smartAccount ? new ethers.BrowserProvider(new AAWrapProvider(smartAccount, SendTransactionMode.Gasless) as Eip1193Provider, "any") : null, [smartAccount]);
+  useEffect(() => {
+    if (smartAccount) {
+      smartAccount
+        .getAddress()
+        .then((address) => {
+          setSmartAccountAddress(address);
+          console.log("@SMART_ADDRESS:", smartAccountAddress);
+        })
+        .catch((error) => console.error("Error getting address:", error));
+    }
+  }, [smartAccount, smartAccountAddress]);
 
-  const NFTContract = useMemo(() => new ethers.Contract(NFT_CONTRACT_ADDRESS, ABI, signer), [signer]);
+  const provider = useMemo(
+    () =>
+      smartAccount
+        ? new ethers.BrowserProvider(
+            new AAWrapProvider(
+              smartAccount,
+              SendTransactionMode.Gasless
+            ) as Eip1193Provider,
+            "any"
+          )
+        : null,
+    [smartAccount]
+  );
+
+  const NFTContract = useMemo(
+    () => new ethers.Contract(NFT_CONTRACT_ADDRESS, ABI, signer),
+    [signer]
+  );
   const VLRContractInterface = useMemo(() => new Interface(VLRABI), []);
 
   useEffect(() => {
@@ -46,53 +90,96 @@ export default function Home() {
 
   const tokenTransfer = useCallback(async () => {
     if (!smartAccount) return;
-    const amount = parseUnits("1", 18);
-    const approveData = VLRContractInterface.encodeFunctionData('approve', [SENDER_ADDRESS, amount]);
-    const transferData = VLRContractInterface.encodeFunctionData('transfer', [SENDER_ADDRESS, amount]);
-    const txs: Transaction[] = [
-      {
-        to: VLR_CONTRACT_ADDRESS,
-        data: approveData
-      },
-      {
-        to: VLR_CONTRACT_ADDRESS,
-        data: transferData
+
+    try {
+      const amount = parseUnits("0.1", 6);
+      const approveData = VLRContractInterface.encodeFunctionData("approve", [
+        smartAccountAddress,
+        amount,
+      ]);
+      const transferData = VLRContractInterface.encodeFunctionData("transfer", [
+        RECEIVER_ADDRESS,
+        amount,
+      ]);
+
+      const txs: Transaction[] = [
+        {
+          to: VLR_CONTRACT_ADDRESS,
+          data: approveData,
+        },
+        {
+          to: VLR_CONTRACT_ADDRESS,
+          data: transferData,
+        },
+      ];
+
+      setTxsString(JSON.stringify(txs, null, 4));
+      console.log("@TRANSACTIONS: ", txs);
+
+      const feeQuotesResult = await smartAccount.getFeeQuotes(txs);
+      console.log("@FEE_QUOTES", feeQuotesResult);
+
+      setFeeQuotesString(JSON.stringify(feeQuotesResult, null, 4));
+
+      const gaslessUserOp = feeQuotesResult.verifyingPaymasterGasless?.userOp;
+      const gaslessUserOpHash =
+        feeQuotesResult.verifyingPaymasterGasless?.userOpHash;
+
+      if (!gaslessUserOp || !gaslessUserOpHash) {
+        throw new Error("Gasless user operation or hash is undefined.");
       }
-    ];
 
-    console.log("txs: ", txs);
-    const feeQuotesResult = await smartAccount.getFeeQuotes(txs);
+      const txHash = await smartAccount.sendUserOperation({
+        userOp: gaslessUserOp,
+        userOpHash: gaslessUserOpHash,
+      });
 
-    const gaslessUserOp = feeQuotesResult.verifyingPaymasterGasless?.userOp;
-    const gaslessUserOpHash = feeQuotesResult.verifyingPaymasterGasless?.userOpHash;
-
-    const txHash = await smartAccount.sendUserOperation({ userOp: gaslessUserOp!, userOpHash: gaslessUserOpHash! });
-
-    console.log("txHash: ", txHash);
-    return txHash;
-  }, [smartAccount])
-
+      console.log("txHash: ", txHash);
+      return txHash;
+    } catch (error) {
+      console.error("Token transfer failed:", error);
+      alert(`Transaction failed: ${error}`);
+    }
+  }, [smartAccount, VLRContractInterface, smartAccountAddress]);
 
   return (
-    <div className='w-screen h-screen flex justify-center items-center flex-col gap-4'>
+    <div className="w-screen h-screen flex justify-center items-center flex-col gap-4">
       <ConnectButton />
-      {
-        isConnected && (
-          <>
-            <h2>Address: {address}</h2>
-          </>
-        )
-      }
-      {
-        isConnected && (
-          <>
-            <button className='p-4 bg-gray-300 rounded-lg' onClick={handleMint}>Mint</button>
+      {isConnected && (
+        <>
+          <h2>Address: {smartAccountAddress}</h2>
+        </>
+      )}
+      {isConnected && (
+        <>
+          <button className="p-4 bg-gray-300 rounded-lg" onClick={handleMint}>
+            Mint
+          </button>
 
-            <div>1 VLR to send {SENDER_ADDRESS}</div>
-            <button className='p-4 bg-gray-300 rounded-lg' onClick={tokenTransfer}>Transfer</button>
-          </>
-        )
-      }
+          <div>Send 1 token to {RECEIVER_ADDRESS}</div>
+          <div>Sender Address {smartAccountAddress}</div>
+          <button
+            className="p-4 bg-gray-300 rounded-lg"
+            onClick={tokenTransfer}
+          >
+            Transfer
+          </button>
+
+          {txsString && (
+            <div className="p-4 bg-gray-100 rounded-lg w-1/2 text-xs overflow-x-auto">
+              <h3 className="font-bold">Transactions:</h3>
+              <pre className="whitespace-pre-wrap">{txsString}</pre>
+            </div>
+          )}
+
+          {feeQuotesString && (
+            <div className="p-4 bg-gray-100 rounded-lg w-1/2 text-xs overflow-x-auto">
+              <h3 className="font-bold">Fee Quotes:</h3>
+              <pre className="whitespace-pre-wrap">{feeQuotesString}</pre>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
